510f56e6fdffbac371375d5717541e79ac163373
Eyad <eabusham2@gmail.com>
2026-07-23 00:38:23 -0500

    Add files via upload

---
+++ snntorch-master/snntorch/_neurons/__init__.py
@@ -0,0 +1,44 @@
+###############################################################
+# When adding new neurons, update the following:              #
+# i) create neuron in snntorch/_neurons/your_neuron.py        #
+# ii) utils.py: reset(); _layer_check(), _layer_reset() etc.  #
+# iii) neurons_dict in backprop.py,                           #
+# iv) update __neuron__ & import below                        #
+# v) unit tests in tests/test_snntorch.py                     #
+# vi) update docs: snntorch.rst                               #
+###############################################################
+
+__neuron__ = [
+    "alpha",
+    "deltaleaky",
+    "lapicque",
+    "leaky",
+    "linearleaky",
+    "stateleaky",
+    "leakyparallel",
+    "rleaky",
+    "rsynaptic",
+    "synaptic",
+    "sconv2dlstm",
+    "slstm",
+    "gen2",
+]
+
+from .neurons import SpikingNeuron
+from .neurons import LIF
+from .alpha import Alpha
+from .deltaleaky import DeltaLeaky
+from .lapicque import Lapicque
+from .leaky import Leaky
+from .linearleaky import LinearLeaky
+from .stateleaky import StateLeaky
+from .associative import AssociativeLeaky
+from .synaptic import Synaptic
+
+from .rleaky import RLeaky
+from .rsynaptic import RSynaptic
+
+from .sconv2dlstm import SConv2dLSTM
+from .slstm import SLSTM
+
+from .leakyparallel import LeakyParallel
